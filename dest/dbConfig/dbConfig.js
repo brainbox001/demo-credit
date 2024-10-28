7065e383527213384c65790abddd9b3169b0278b
Daniel Marcus <130494114+brainbox001@users.noreply.github.com>
2024-10-28 09:30:20 +0100

    Delete dest directory

--- dest/dbConfig/dbConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const knexfile_js_1 = __importDefault(require("./knexfile.js"));
-const knex_1 = __importDefault(require("knex"));
-const db = (0, knex_1.default)(knexfile_js_1.default.development);
-exports.default = db;
