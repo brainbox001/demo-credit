a62c89fe7cb2745dbe13515fc09ac02adbcd1ddc
Daniel Marcus <momsdboy@gmail.com>
2024-11-02 13:38:14 +0100

    updated user route to innclude login

--- src/routers/userRoutes.ts
+++ src/routers/userRoutes.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import registerObj from '../controllers/users/register';
+import login from '../controllers/users/login';
 
 const {register} = registerObj;
 
@@ -7,5 +8,6 @@ const userRoutes = express.Router()
 userRoutes.use(express.json());
 
 userRoutes.post('/register', register);
+userRoutes.post('/login', login);
 
 export default userRoutes;
